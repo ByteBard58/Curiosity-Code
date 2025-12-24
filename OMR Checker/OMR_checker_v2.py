7083e93e7021dc639f20114af8ca9be408263aa7
Sakib Hossain <sakibhossainalif29@gmail.com>
2025-12-24 10:07:33 +0600

    Update OMR Checker version2

--- OMR Checker/OMR_checker_v2.py
+++ OMR Checker/OMR_checker_v2.py
@@ -1,28 +1,35 @@
 import time
-import numpy as np
+
 
 # Solutions
 gt = []
+target = int(input(">>> How many questions are there in you question set? : "))
+for i in range(target):
+  while True:
+    ans = input(f"Input the answer of question {i+1}: ")
+    if ans in ["A","B","C","D"]:
+      gt.append(ans)
+      break
+    print('Allowed inputs are ["A","B","C","D"]')
 
 points = 0
 neat = []
 print("Choose from A,B,C and D. Input 0(zero) to skip.")
 start = time.time()
 for num,i in enumerate(gt):
-  user = input(f">>> Input your answer for Question No.{num + 1}: ")
-  if user not in ["A","B","C","D","0"]:
+  while True:
+    user = input(f">>> Input your answer for Question No.{num + 1}: ")
+    if user in ["A","B","C","D","0"]:
+      break
     print("Please input A,B,C or D (in capital form)")
-    break
-  elif user == i:
+
+  if user == i:
     print("Correct Answer !")
     points += 1
-  elif user != i:
-    print("Wrong Answer !")
   elif user == "0":
     print("Question Skipped")
-  else: 
-    print(f"Something went wrong. Iteration Number = {num}")
-    break
+  else:
+    print("Wrong Answer !")
 end = time.time()
 elapsed = end - start
 mins, secs = divmod(elapsed, 60)
