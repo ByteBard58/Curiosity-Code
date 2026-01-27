923773b515cc11044f394c0a9209f55dc8ae718e
Sakib Hossain <sakibhossainalif29@gmail.com>
2026-01-27 11:05:26 +0600

    Implement and plot golden ratio approximations using continued fractions and square root iteration methods.

--- Phi/phi_hunt.py
+++ Phi/phi_hunt.py
@@ -3,8 +3,10 @@ import numpy as np
 import time
 import matplotlib.pyplot as plt
 
+# True value of phi
 phi_main: float = golden
 
+# Function to generate the fibonacchi series and calculate the golden ratio
 def fibon(n) -> tuple[list[int],list[float],list[int]]:
   if n == 0 or n == 1:
     raise ValueError(f"Fibonacchi series requires at lease 2 elements, {n} was set")
@@ -25,16 +27,65 @@ def fibon(n) -> tuple[list[int],list[float],list[int]]:
     golden_ratio_x.append(i)
     elements.append(prv)
   end: float = time.time()
-  print(f"Time Taken {(end-strt):.5f} Second")
+  print(f"Time Taken(Fibonacchi Series) {(end-strt):.5f} Second")
   
   return elements,golden_ratio,golden_ratio_x
 
 elms,gold,gold_x = fibon(1000)
 
+# Function to generate continued fraction of phi and generate the golden ratio
+def cont_frac(n):
+  element:list[float] = []
+  element_x:list[int] = [0]
+  
+  strt: float = time.time()
+  for i in range(n):
+    if i == 0:
+      x = 1+(1/1)
+      element.append(x)
+      continue
+    x = sum(element[-1:])
+    outp = 1+(1/x)
+    element.append(outp)
+    element_x.append(i)
+  end: float = time.time()
+  print(f"Time Taken(Continued Fraction) {(end-strt):.5f} Second")
+  
+  return element,element_x
+
+gold_cont,gold_cont_x = cont_frac(1000)
+
+# Function to generate the golden ratio from Square root iteration
+def sqrt_iter(n):
+  elements:list[float] = []
+  elements_x:list[int] = [0]
+
+  strt: float = time.time()
+  for i in range(n):
+    if i == 0:
+      x = np.sqrt(1+np.sqrt(1))
+      elements.append(x)
+      continue
+    x = sum(elements[-1:])
+    output = np.sqrt(1+x)
+    elements.append(output)
+    elements_x.append(i)
+  end: float = time.time()
+  print(f"Time Taken(Square Root Iteration) {(end-strt):.5f} Second")
+
+  return elements,elements_x
+
+sqrt_elm,sqrt_elm_x = sqrt_iter(1000)
+
+# Plotting the graph
 plt.figure(figsize=(9,6))
 
 plt.plot(gold_x,gold, color="orange",
-            marker="s",markersize=6, label="Approximated Value")
+            marker="s",markersize=3, label="Fibonacchi Series")
+plt.plot(gold_cont_x,gold_cont, color="green",
+            marker="o",markersize=3, label="Continued Fraction")
+plt.plot(sqrt_elm_x,sqrt_elm, color="red",
+            marker="^",markersize=3, label="Square Root Iteration")
 plt.axhline(y=phi_main, color="blue", linestyle="--", linewidth=2,
             label=f"True φ = {phi_main:.12f}")
 
@@ -45,4 +96,3 @@ plt.xscale("log")
 plt.legend()
 plt.grid()
 plt.show()
-
