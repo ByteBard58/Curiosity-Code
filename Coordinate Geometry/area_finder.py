c5a7c7fb964ab14525daf4c35a9024833e7c497c
Sakib Hossain <sakibhossainalif29@gmail.com>
2026-02-13 20:49:26 +0600

    Add convex hull logic to solve the potential order mismatch issue

--- Coordinate Geometry/area_finder.py
+++ Coordinate Geometry/area_finder.py
@@ -12,7 +12,29 @@ area. A demo is provided.
 
 import numpy as np 
 
+def cross(o, a, b):
+    return (a[0] - o[0]) * (b[1] - o[1]) - \
+           (a[1] - o[1]) * (b[0] - o[0])
+
+def convex_hull(points):
+    points = sorted(points.tolist())
+    
+    lower = []
+    for p in points:
+        while len(lower) >= 2 and cross(lower[-2], lower[-1], p) <= 0:
+            lower.pop()
+        lower.append(p)
+
+    upper = []
+    for p in reversed(points):
+        while len(upper) >= 2 and cross(upper[-2], upper[-1], p) <= 0:
+            upper.pop()
+        upper.append(p)
+
+    return np.array(lower[:-1] + upper[:-1])
+
 def area_sc(target) -> np.float64:
+  target = convex_hull(target)
   x: np.ndarray = target[:,0]
   y: np.ndarray = target[:,1]
 
